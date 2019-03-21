694f077194063ec7b3ba77aa832bf3a9c00f3c40
AndrewZhuZJU <binzhubz@zju.edu.cn>
2019-03-21 10:32:33 +0800

    update args

--- args.py
+++ args.py
@@ -8,7 +8,7 @@ def get_parser():
 
     parser.add_argument('--num_workers', type=int, help='number of data loading workers', default=8)
     parser.add_argument('--batch_size', type=int, default=256, help='input batch size')
-    parser.add_argument('--num_epochs', type=int, default=100, help='number of epochs to train for')
+    parser.add_argument('--num_epochs', type=int, default=60, help='number of epochs to train for')
     parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
     
     parser.add_argument('--netG', default='', help="path to netG (to continue training)")
@@ -17,6 +17,9 @@ def get_parser():
     parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
     parser.add_argument('--ngf', type=int, default=64)
     parser.add_argument('--ndf', type=int, default=64)
+    parser.add_argument('--ncritic', type=int, default=5, help='WGAN-number of critic parameter per generator iteration')
+    parser.add_argument('--clip', type=float, default=0.01, help='WGAN-weight clipping')
+    parser.add_argument('--lambda_gp', type=int, default=10, help='WGAN_GP-gradient penalty parameter')
     parser.add_argument('--num_label', type=int, default=2)
     parser.add_argument('--checkpoints', default='checkpoints/', help='folder to output images and model checkpoints')
     return parser
